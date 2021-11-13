8ef894b226981160a34d9e85f25ca190412a1aaf
Michael Probst <mpprobst@gmail.com>
2021-11-12 23:15:34 -0500

    fix

--- server.c
+++ server.c
@@ -253,11 +253,9 @@ read_output * read_file_1_svc(read_input *argp, struct svc_req *rqstp) {
 			num_bytes_to_read = available_space;
 		}
 
-		char * buf = &file.data[entry.fp];
-		printf("made buf at file pointer %s\n", buf);
 		char * buffer = (char*)malloc(num_bytes_to_read);
-		printf("buffer allocated %d bytes\n", sizeof(num_bytes_to_read));
-		memcpy(buffer, &file.data[entry.fp], num_bytes_to_read);
+		printf("buffer allocated %d bytes\n", sizeof(buffer));
+		memcpy(buffer, file.data[entry.fp], num_bytes_to_read);
 		entry.fp+=num_bytes_to_read;
 		entry.op = 1;
 		printf("prepared buffer: %s\b", buffer);
