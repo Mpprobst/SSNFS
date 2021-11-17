4cd3d877e41378121cc60446b125aa8d392c067a
Michael Probst <mpprobst@gmail.com>
2021-11-17 14:15:16 -0500

    f

--- server.c
+++ server.c
@@ -348,8 +348,10 @@ write_output * write_file_1_svc(write_input *argp, struct svc_req *rqstp)
 			// write to blocks 512 bytes at a time
 			lseek(mem, fi.blocks[curr_block]*BLOCK_SIZE+idx, SEEK_SET);
 			write(mem, &argp->buffer.buffer_val+bytes_written, bytes_to_write);
+			bytes_written += bytes_to_write;
 			fi.curr_size += bytes_written;
 		}
+		close(mem);
 		table[argp->fd].fp += bytes_written;
 	}
 	return &result;
